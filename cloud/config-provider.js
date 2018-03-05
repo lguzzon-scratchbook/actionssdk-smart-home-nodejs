ef772ea23c3c60f5835a34bbd5fde5c747d82bd2
Luca Guzzon <luca.guzzon@gmail.com>
2018-03-05 15:18:33 +0100

    smartHomeProviderApiKey from env

--- cloud/config-provider.js
+++ cloud/config-provider.js
@@ -16,7 +16,7 @@ var Config = {};
 Config.devPortSmartHome = "3000";
 Config.smartHomeProviderGoogleClientId = "ZxjqWpsYj3"; // client id that Google will use
 Config.smartHomeProvideGoogleClientSecret = "hIMH3uWlMVrqa7FAbKLBoNUMCyLCtv"; // client secret that Google will use
-Config.smartHomeProviderApiKey = "<API_KEY>"; // client API Key generated on the console
+Config.smartHomeProviderApiKey = process.env.smartHomeProviderApiKey; // client API Key generated on the console
 Config.isLocal = false;
 
 function init() {
